149aa8d28a252d0491131aced71c38479949f099
Admin <vanpho02@gmail.com>
2017-03-16 21:27:25 +0700

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+// let a = 'Xin chao';
+
+// let a = 10;
+// let b = '10';
+
+// console.log( a !== b );
+
+let a = true;
+let b = false;
+let c = 10;
+// console.log(c /= 5);
+
+let d = 10;
+let e = 10;
+// 0, null, undefined, false
+
+    // if(aVar) {
+    //     max = d;
+    // } else {
+    //     max = e;
+    // }
+
+// let max = d - e ? d : e + 1;
+
+// console.log( typeof null );
+
+// console.log(max);
+let g = 100;
+let h = 101;
+// console.log((g++, g + h));
+
+if(null) console.log("Xin chao ban");
+
---
+++ index2.js
@@ -0,0 +1,19 @@
+// for (console.log(1); console.log(2); console.log(3)) {
+//     console.log('h');
+// }
+
+// let i = 0;
+
+// while (true) {
+//     console.log(i++);
+//     if(i == 6) break;
+// }
+
+let a = 5, b = 10;
+
+if(a > 100 & b++ > 5) {
+    console.log('DUNG', b);
+} else {
+    console.log('DUNG', b);
+}
+
